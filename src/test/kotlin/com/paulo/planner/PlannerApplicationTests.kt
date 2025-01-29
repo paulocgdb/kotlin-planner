db81841da1f6608ea6a9f8f341b6fae54b8fdf60
paulo.brito <paulocesarti01@gmail.com>
2025-01-29 17:50:19 -0300

    config: adding initial config files

---
+++ src/test/kotlin/com/paulo/planner/PlannerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.paulo.planner
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+//	@Test
+//	fun contextLoads() {
+//	}
+
+}
